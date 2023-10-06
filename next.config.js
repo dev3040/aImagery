6a483da6f2d8ea82571bde742de470735e3bbbef
Devang Patel <devangp@itpathsolutions.com>
2023-10-06 15:03:21 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    API_URL: "http://15e9-34-32-234-177.ngrok-free.app"
+    API_URL: "https://15e9-34-32-234-177.ngrok-free.app"
 }
 
 module.exports = nextConfig
