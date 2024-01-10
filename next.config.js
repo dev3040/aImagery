53946ec406a6bc465e578b1ae96bb6798890a7af
Karan Pal <karanp@itpathsolutions.com>
2024-01-10 14:11:07 +0530

    changessssss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    API_URL: "https://aimageryv2.project-demo.info",
+    API_URL: "https://7de2-122-169-115-137.ngrok-free.app",
     BACKEND_URL: "https://aimageryapi.project-demo.info:3004/v1",
-    PROMP_URL: "https://aimageryv2.project-demo.info"
+    PROMP_URL: "https://7de2-122-169-115-137.ngrok-free.app"
 }
 
 module.exports = nextConfig
