f281ca127a0aaf10d27aca887ed13fd2f7ccd586
KaranPalIps <107114583+KaranPalIps@users.noreply.github.com>
2024-03-12 10:34:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    API_URL: "https://edb4-122-169-115-137.ngrok-free.app",
+    API_URL: "https://f0a6-122-169-115-137.ngrok-free.app",
     BACKEND_URL: "https://aimageryapi.project-demo.info:3004/v1",
-    PROMP_URL: "https://edb4-122-169-115-137.ngrok-free.app"
+    PROMP_URL: "https://f0a6-122-169-115-137.ngrok-free.app"
 }
 
 module.exports = nextConfig
